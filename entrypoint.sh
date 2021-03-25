3509369d6434e8b594d7b55fe9601cf0696355a7
egallup02360 <41955146+egallup02360@users.noreply.github.com>
2021-03-25 01:07:48 -0400

    Add favicon

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,9 @@
 # sometimes. In this case, following checks will fail and wifi-connect
 # will be launched even if the device will be able to connect to a WiFi network.
 # If this is your case, you can wait for a while and then check for the connection.
-wifi-connect --help 2>&1
-if [ $? -ne 0 ]; then
-    bash <(curl -L https://github.com/balena-io/wifi-connect/raw/master/scripts/raspbian-install.sh)
-fi
+sleep 20
 
-#sleep 15
-
-wget --spider http://google.com 2>&1
+nmcli -t g | grep full
 
 if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
@@ -27,4 +22,4 @@ rbenv exec bundle exec bundle install
 yarn install
 rbenv exec bundle exec rails assets:precompile
 rbenv exec bundle exec rails webpacker:compile
-rbenv exec bundle exec rails s -e production -p 80
+service lottominer-web restart
