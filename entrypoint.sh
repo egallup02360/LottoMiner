db47d5bd2496ab27afa001a860d829f475bd41fd
egallup02360 <41955146+egallup02360@users.noreply.github.com>
2021-04-05 16:29:08 -0400

    Keep LED status

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,3 +23,6 @@ yarn install
 rbenv exec bundle exec rails assets:precompile
 rbenv exec bundle exec rails webpacker:compile
 service lottominer-web restart
+if [ ! -f "LEDS_OFF" ]; then
+  service leds restart
+fi
