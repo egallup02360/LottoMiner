368afd93233d7e3a8f3d1734936e55c29a5a796f
egallup02360 <41955146+egallup02360@users.noreply.github.com>
2021-04-05 16:29:44 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,5 +24,5 @@ rbenv exec bundle exec rails assets:precompile
 rbenv exec bundle exec rails webpacker:compile
 service lottominer-web restart
 if [ ! -f "LEDS_OFF" ]; then
-  service leds restart
+  service leds stop
 fi
