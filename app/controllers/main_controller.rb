99d07bed61454a214aa0c6e93394c71d36e694e6
egallup02360 <41955146+egallup02360@users.noreply.github.com>
2021-04-14 19:27:20 -0400

    Wifi config

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,6 +9,12 @@ class MainController < ApplicationController
     end
   end
 
+  def reconfigure_wifi
+    fork { system("sh #{Rails.root.join('reconfigure_wifi.sh')}") }
+    flash[:success] = "This webpage will be shut down shortly. Then you will need to connect to the network 'LottoMiner-Config' to reconfigure your wifi connection."
+    redirect_to root_url
+  end
+
   def api_action
     payload = params[:payload]
     @response = CgminerApi.call(payload)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :pools, except: [:create]
   get '/toggle_leds', to: 'main#toggle_leds'
+  get '/reconfigure_wifi', to: 'main#reconfigure_wifi'
   get "/pools/:id/make_active", to: 'pools#make_active', as: 'make_pool_active'
   get "api_action/:payload", to: 'main#api_action', as: 'api_action'
 
